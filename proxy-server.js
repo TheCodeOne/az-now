656fa3f4432720ae3d055ea0135e214b966a16c1
Dimitrios Kokkonias <dimi.kokkonias@gmail.com>
2024-12-14 16:08:26 +0100

    🐛 Fix port

--- proxy-server.js
+++ proxy-server.js
@@ -134,7 +134,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/az-now/browser/index.html"));
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
